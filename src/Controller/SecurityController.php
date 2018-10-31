08c4eded94b8bb558c5bd39217b0b6ac63d98ac0
Alexandra <alyakh2014@gmail.com>
2018-10-31 21:54:45 +0300

    User login/logout/register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,20 +7,25 @@
  */
 
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\UserRepository;
+use App\Entity\User;
 
-class SecurityController extends Controller
+class SecurityController extends AbstractController
 {
+
+    private $security;
     /**
      * @param Request $request
      * @Route("/login", name="login")
      */
     public function loginAction(
         Request $request,
-        AuthenticationUtils $authUtils)
+        AuthenticationUtils $authUtils): Response
     {
         // получить ошибку входа, если она есть
         $error = $authUtils->getLastAuthenticationError();
@@ -32,5 +37,29 @@ class SecurityController extends Controller
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
+    }
+
+    /**
+     * @Route("/account", name="account")
+     */
+    public function redirectToAccount(Request $request): Response
+    {
+        $user = new User();
+        return new Response(
+            "<h1>".$user->getEmail()."</h1>"
+        );
+    }
+
+    /**
+     * This is the route the user can use to logout.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the logout automatically. See logout in config/packages/security.yaml
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
     }
 }
